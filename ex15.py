b226f8e92f5d6b80eb1ac88e0344703c0dd7e21a
hcchen11 <hcchen11@users.noreply.github.com>
2017-04-28 13:44:39 -0400

    Update ex15.py

--- ex15.py
+++ ex15.py
@@ -1,1 +1,13 @@
 
+# open and read file from python Shell
+# create a file object
+file = open("ex15_sample.txt", "r")
+# read the content
+print file.read()
+
+# read the first 4 characters
+file = open("ex15_sample.txt", "r")
+print file.read(4)
+file.close()
+
+
