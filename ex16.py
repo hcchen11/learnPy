6cb49d14be623255c051bac5ceb8be12ad3882b6
hcchen11 <hcchen11@users.noreply.github.com>
2017-04-29 19:03:25 -0400

    Create ex16.py

---
+++ ex16.py
@@ -0,0 +1,5 @@
+# close : close the file
+# read : reads the contents of the file, can assign the result to a variable
+# readline : read just one line of a text file
+# truncate: empties the file. 
+# write('stuff') : write "stuff" to the file
